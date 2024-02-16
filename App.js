e47f7da89619c6799422022ca6fa2d1af52a91a7
Simranjit Singh <simranjitsingh4661@gmail.com>
2024-02-17 02:08:29 +0530

    firebase added, packages added

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation';
+// import {enableLatestRenderer} from 'react-native-maps';
+
+// enableLatestRenderer();
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
